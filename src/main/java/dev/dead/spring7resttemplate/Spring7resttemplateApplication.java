55e2a4baa23dabbf2e148a8babf8876897e4ed40
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2025-12-02 17:51:18 +0300

    initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/main/java/dev/dead/spring7resttemplate/Spring7resttemplateApplication.java
@@ -0,0 +1,13 @@
+package dev.dead.spring7resttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7resttemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring7resttemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dead/spring7resttemplate/Spring7resttemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dead.spring7resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7resttemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
