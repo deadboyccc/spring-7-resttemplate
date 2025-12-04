28e50b3da20a15cb9492cf0d869ca4a71c1001f2
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2025-12-04 13:30:35 +0300

    refactor: simplify BeerClientImpl by using RestTemplate directly and remove unused RestTemplateBuilder

--- src/main/java/dev/dead/spring7resttemplate/Spring7resttemplateApplication.java
+++ src/main/java/dev/dead/spring7resttemplate/Spring7resttemplateApplication.java
@@ -2,12 +2,19 @@ package dev.dead.spring7resttemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Spring7resttemplateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring7resttemplateApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
--- src/main/java/dev/dead/spring7resttemplate/client/BeerClientImpl.java
+++ src/main/java/dev/dead/spring7resttemplate/client/BeerClientImpl.java
@@ -3,22 +3,26 @@ package dev.dead.spring7resttemplate.client;
 import dev.dead.spring7resttemplate.models.BeerDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+@Component
 @RequiredArgsConstructor
 @Slf4j
 public class BeerClientImpl implements BeerClient {
-    private final RestTemplateBuilder restTemplateBuilder;
+    private final RestTemplate restTemplate;
 
     @Override
     public Page<BeerDTO> listBeers() {
-        RestTemplate restTemplate = restTemplateBuilder.build();
-        ResponseEntity<String> responseEntity =
-                restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/v1/beer",
+                String.class);
 
+        log.info("Status Code: {}", responseEntity.getStatusCode());
+        log.debug("Response Body: {}", responseEntity.getBody());
+
+        // TODO: Parse the JSON response and convert to Page<BeerDTO>
         return null;
     }
 }
